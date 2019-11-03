b69dccbf42f1a585886006cab0edb2080d28991a
David Schawel <david.schawel@gmail.com>
2019-11-03 15:40:17 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+const cards = ['queen', 'queen', 'king', 'king'];
+const cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne)
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo)
+
+console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardFour);
+
+if(cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}

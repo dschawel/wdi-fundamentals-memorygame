3e0f6f582ad1e3628edc43a828550c23b8bc88ff
David Schawel <david.schawel@gmail.com>
2019-11-04 11:23:22 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,21 +1,46 @@
-const cards = ['queen', 'queen', 'king', 'king'];
+const cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+	];
 const cardsInPlay = [];
-cardsInPlay.push(cards[0], cards[2])
+cardsInPlay.push(cards[0].rank, cards[2].rank)
 
 function checkForMatch() {
 if (cardsInPlay[0] === cardsInPlay[1]) {
-	console.log("You found a match!");
+	alert("You found a match!");
 } else {
-	console.log("Sorry, try again.");
+	alert("Sorry, try again.");
 	}
 };
 
 function flipCard(cardId) { 
-	console.log("User flipped " + cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 };
 	flipCard(0);
 	flipCard(2); 
 	checkForMatch();
+
+
 
 
 
